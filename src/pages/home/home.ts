277f641217f35cf8a4573c89f9507a2389e5d24b
Adam Swanson <AMSwanson01@indianatech.net>
2018-09-28 12:37:43 -0400

    Basic search functionality.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
+
+  }
+
+  results: any;
+
+  searchFunction(q: string) {
+    this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + q.replace(/[^a-zA-Z ]/g, "")).map(res => res.json()).subscribe(
+      data => {
+        //this.results = data.items[0].volumeInfo.title;
+        this.results = data;
+
+        var titles:string = "";
+
+        for (var item in this.results.items) {
+          titles += this.results.items[item].volumeInfo.title + "\n";
+        }
+
+        alert(titles);
+      },
+      err => {
+        console.log("Error in searchFunction.");
+        alert("Error in searchFunction.");
+      });
+
 
   }
 
