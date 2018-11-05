59eaceea8ace6953c5940b90d89ece0caeeb7186
kdalqarni <kdalqarni@gmail.com>
2018-11-05 08:54:00 -0500

    Created hamburger Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { LibraryPage } from '../pages/library/library';
+import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = LoginPage;
+  pages: Array<{title: string, component: any}>;
+ 
+  
+  constructor(public platform: Platform) {
+    this.pages = [
+      { title: 'Search', component: HomePage },
+      { title: 'Library', component: LibraryPage },
+      { title: 'Settings', component: SettingsPage },
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+
+    })
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
   }
 }
