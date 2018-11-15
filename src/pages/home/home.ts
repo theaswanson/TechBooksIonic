9fe6522e0e6116c59b270d6e2de19d5c2438cabb
Bart Peter <bartpeter@outlook.com>
2018-11-15 14:50:42 -0500

    book logic abstraction complete for HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,16 +15,11 @@ import { BookLogicProvider } from '../../providers/book-logic/book-logic';
 })
 export class HomePage {
 
-  results:any;
-  books:any;
-  noResults:any;
-  loading:any;
+
   logoTaps:number;
 
   constructor(public logic:BookLogicProvider, public navCtrl: NavController, public http: Http, private keyboard: Keyboard, public modalCtrl : ModalController) {
-    this.books = [];
-    this.noResults = false;
-    this.loading = false;
+ 
     this.logoTaps = 0;
   }
 
@@ -34,13 +29,6 @@ export class HomePage {
     })
   }
 
-  openKeyboard() {
-    this.keyboard.show();
-  }
-
-  closeKeyboard() {
-    this.keyboard.hide();
-  }
 
   logoTap(e)
   {
@@ -58,41 +46,5 @@ export class HomePage {
     modalPage.present(); 
   }
 
-  searchFunction(q: string) {
-    this.closeKeyboard();
 
-    this.books = [];
-    this.loading = true;
-    this.noResults = false;
-
-    if (q.replace(/\s/g, '').length == 0) {
-      this.noResults = true
-      this.loading = false
-    } else {
-
-      this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + q.replace(/[^a-zA-Z1-9 ]/g, "")).map(res => res.json()).subscribe(
-        data => {
-
-          this.results = data;
-
-          for (var item in this.results.items) {
-            if (this.results.items[item].volumeInfo.hasOwnProperty('imageLinks') && this.results.items[item].volumeInfo.hasOwnProperty('description')) {
-              this.books.push(this.results.items[item]);
-            }
-          }
-          if (this.books.length == 0) {
-            this.noResults = true;
-          }
-
-          this.loading = false;
-
-          console.log(this.books);
-
-          err => {
-            console.log("Error in searchFunction.");
-            alert("Error in searchFunction.");
-          }
-        });
-    }
-  }
 }
